2134f84f7190c3f5f6cff38f884cffbc9a674b0a
danikonova <124899052+danikonova@users.noreply.github.com>
2023-05-20 12:02:57 +0400

    Update lab6_part2.py

--- lab6_part2.py
+++ lab6_part2.py
@@ -5,7 +5,6 @@
 вариант 21
 '''
 from fractions import Fraction
-from itertools import combinations
 
 numbers = [3, 5, 7, 11, 13, 17]
 fractions = set()
@@ -22,17 +21,21 @@ max_fractions = []
 max_sum = 0
 
 #Итерация по всем возможным комбинациям дробей из fractions
-for a, b in combinations(fractions, 2):
-    a_num, a_den = a.as_integer_ratio()                                   #получаем числитель и знаменатель первой дроби из комбинации.
-    b_num, b_den = b.as_integer_ratio()                                   #получаем числитель и знаменатель второй дроби из комбинации.
-    if ((a_num + b_num) % 2 == 0) and ((a_den + b_den) % 2 == 0):
-        num_sum = int(str(a_num+b_num)[-1]) + int(str(a_den+b_den)[-1])   #складываем последние цифры суммы числителей и знаменателей.
-        if num_sum % 5 == 0:
-            if abs(num_sum) > abs(max_sum):
-                max_sum = num_sum
-                max_fractions = [(a, b)]
-            elif abs(num_sum) == abs(max_sum):
-                max_fractions.append((a, b))
+fractions_list = list(fractions)
+for i in range(len(fractions_list)):
+    for j in range(i+1, len(fractions_list)):
+        a = fractions_list[i]
+        b = fractions_list[j]
+        a_num, a_den = a.as_integer_ratio()
+        b_num, b_den = b.as_integer_ratio()
+        if ((a_num + b_num) % 2 == 0) and ((a_den + b_den) % 2 == 0):
+            num_sum = int(str(a_num+b_num)[-1]) + int(str(a_den+b_den)[-1])
+            if num_sum % 5 == 0:
+                if abs(num_sum) > abs(max_sum):
+                    max_sum = num_sum
+                    max_fractions = [(a, b)]
+                elif abs(num_sum) == abs(max_sum):
+                    max_fractions.append((a, b))
 
 #Вывод результата - максимальной суммы и пары дробей
 print("Максимальная сумма по модулю 5:", abs(max_sum))
